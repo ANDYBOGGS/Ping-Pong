daa713985cb904c2de69952e740da03695248af4
Andy Boggs <ajacobboggs@gmail.com>
2018-03-08 13:06:17 -0800

    add background image, remove jumbotron, add h1 css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,3 @@
-$(document).ready(function() {
-  $('#input form').submit(function(event) {
-    event.preventDefault();
-    debugger;
-    var bigNum = parseInt($('input#userInput').val());
-    var multipleNum = parseInt($('input#countBy').val());
-    var multiples = [];
-
-      for (var i = multipleNum; i <= bigNum; i += multipleNum) {
-
-        if (bigNum % i === 0) {
-          multiples.push(i);
-        };
-
-
-     $('#output').text(multiples);
-  };
- });
-});
 
 //var outputArr =[];
 var ul = $('#output ul');
