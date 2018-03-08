39b7eae6fda9783a81d38488fab0c8ca520ad2a1
Andy Boggs <ajacobboggs@gmail.com>
2018-03-08 12:17:21 -0800

    add initial javascript code, html input/submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
-var numbers = [Joe, Sam, Ed, Betty, Seth];
+$(document).ready(function() {
+  $('#input form').submit(function(event) {
+    event.preventDefault();
+    debugger;
+    var bigNum = parseInt($('input#userInput').val());
+    var multipleNum = parseInt($('input#countBy').val());
+    var multiples = [];
 
-var doubleNumbers = numbers.map(function(numbers) {
-  return numbers.val().toUpperCase();
+      for (var i = multipleNum; i <= bigNum; i += multipleNum) {
+
+        if (bigNum % i === 0) {
+          multiples.push(i);
+        };
+
+
+     $('#output').text(multiples);
+  };
+ });
 });
-
-alert(doubleNumbers);
