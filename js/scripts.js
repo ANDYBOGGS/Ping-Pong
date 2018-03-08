9b2a0fce68f897de9ed5abde71fafa6a96a97ea9
Andy Boggs <ajacobboggs@gmail.com>
2018-03-08 12:34:44 -0800

    add readme content and additioanl JS script

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,34 @@ $(document).ready(function() {
   };
  });
 });
+
+//var outputArr =[];
+var ul = $('#output ul');
+function countUp(numInput) {
+  const outputArr =[];
+  for (var i = 1; i <= numInput; i++) {
+    if (i % 15 === 0) {
+      outputArr.push("Ping-Pong");
+    } else if (i % 5 === 0) {
+      outputArr.push("pong");
+    } else if (i % 3 === 0) {
+      outputArr.push("Ping");
+    } else {
+      outputArr.push(i);
+    };
+
+  };
+  return outputArr;
+};
+
+
+$(document).ready(function() {
+  $("#main-content form").submit(function(event) {
+    event.preventDefault();
+    var numInput = parseInt($("input#userInput").val());
+    var result = countUp(numInput);
+
+    for ()
+    $("#output").text(result);
+  });
+});
